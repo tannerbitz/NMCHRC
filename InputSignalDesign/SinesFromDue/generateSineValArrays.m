b1ff13446bc40883980108da0fffde4f60bb90f1
Tanner Bitz <tannerbitz@gmail.com>
2018-08-21 13:13:51 -0700

    added 5th sine wave to input signal -- freq = 5/4 hz

--- InputSignalDesign/SinesFromDue/generateSineValArrays.m
+++ InputSignalDesign/SinesFromDue/generateSineValArrays.m
@@ -1,4 +1,4 @@
-freq = [1/4, 1/2, 1, 2];
+freq = [1/4, 1/2, 1, 5/4, 2];
 
 for i = 1:length(freq)
     for sample = 0:(1000/freq(i) - 1)
@@ -7,10 +7,12 @@ for i = 1:length(freq)
 end
 
 %%
+
 sinequarterhz = sineval(1, 1:4000);
 sinehalfhz = sineval(2, 1:2000);
 sineonehz = sineval(3, 1:1000);
-sinetwohz = sineval(4, 1:500);
+sinefivequartershz = sineval(4, 1:800);
+sinetwohz = sineval(5, 1:500);
 
 
 %%
@@ -46,4 +48,13 @@ for i = 1:125
                                      sinetwohz((4*(i-1)+2)), ...
                                      sinetwohz((4*(i-1)+3)), ...
                                      sinetwohz((4*(i-1)+4)));
+end
+
+%% 
+fid = fopen('sinefivequartershz.txt', 'w');
+for i = 1:length(sinefivequartershz)/4
+   fprintf(fid, "%i, %i, %i, %i,\n", sinefivequartershz((4*(i-1)+1)), ...
+                                     sinefivequartershz((4*(i-1)+2)), ...
+                                     sinefivequartershz((4*(i-1)+3)), ...
+                                     sinefivequartershz((4*(i-1)+4)));    
 end

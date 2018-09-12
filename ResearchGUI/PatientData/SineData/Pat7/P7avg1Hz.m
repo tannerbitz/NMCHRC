cbd1510c5ab1f0c8a82bc93a0a63be1e824fd377
Cory Jernigan <coryjernigan@Corys-MacBook-Pro-2.local>
2018-09-11 17:16:29 -0700

    Updated Sine Analysis

--- ResearchGUI/PatientData/SineData/Pat7/P7avg1Hz.m
+++ ResearchGUI/PatientData/SineData/Pat7/P7avg1Hz.m
@@ -2,7 +2,7 @@ clear;
 clc;
 
 p7mvcdf = load('Patient7_MVC_DF.txt');
-p7df25 = load('PatNo7_VR_AnklePosNeutral_DF_0-25Hz.txt');
+p7df25 = load('PatNo7_VR_AnklePosNeutral_DF_1-0Hz.txt');
 
 p7df_m = p7df25(:,1);
 p7df_ref = p7df25(:,2);
@@ -10,19 +10,20 @@ p7df_ref = p7df25(:,2);
 serial2lbs_bipolar = 125/2048;
 lbs2NmAt15cm = 4.448*.15;
 
-n = 1;
-for i = 1:16
-   
-    df(:,i) = p7df_m(n:4000+n);
-    n = n + 4000;
+n = 187;
+for i = 1:64
+    
+    df(:,i) = p7df_m(n:1000+n);
+    n = n + 1000;
+ 
     
 end
 
 dfm = mean(df,2);
 
 SDdfm = std(dfm);
-SDdfmP = dfm + 29.4304;
-SDdfmM = dfm - 29.4304;
+SDdfmP = dfm + SDdfm;
+SDdfmM = dfm - SDdfm;
 
 mvcdf1 = max(p7mvcdf(5500:9500));
 mvcdf2 = max(p7mvcdf(25500:29500));
@@ -31,22 +32,14 @@ mvcdf3 = max(p7mvcdf(45500:49500));
 mvcdfSerial = max([mvcdf1, mvcdf2, mvcdf3]);
 mvcdf_Nm = abs(mvcdfSerial)*serial2lbs_bipolar*lbs2NmAt15cm;
 
-refsig = p7df_ref(1:4001);
+refsig = p7df_ref(187:1190);
 refsigser2Nm = mvcdf_Nm/4096*0.2;
 
 ref = refsig*refsigser2Nm;
 
 hold on;
-title('1/4 Hz Dorsiflexion');
+title('1 Hz Dorsiflexion');
 plot(dfm*serial2lbs_bipolar*lbs2NmAt15cm+8.25);
 plot(ref);
 plot(SDdfmP*serial2lbs_bipolar*lbs2NmAt15cm+8.25);
 plot(SDdfmM*serial2lbs_bipolar*lbs2NmAt15cm+8.25);
-
-
-
-
-
-
-
-
---
+++ ResearchGUI/PatientData/SineData/Pat7/P7avg50Hz.m
@@ -0,0 +1,45 @@
+clear;
+clc;
+
+p7mvcdf = load('Patient7_MVC_DF.txt');
+p7df25 = load('PatNo7_VR_AnklePosNeutral_DF_0-50Hz.txt');
+
+p7df_m = p7df25(:,1);
+p7df_ref = p7df25(:,2);
+
+serial2lbs_bipolar = 125/2048;
+lbs2NmAt15cm = 4.448*.15;
+
+n = 1;
+for i = 1:32
+    
+    df(:,i) = p7df_m(n:2000+n);
+    n = n + 2000;
+ 
+    
+end
+
+dfm = mean(df,2);
+
+SDdfm = std(dfm);
+SDdfmP = dfm + SDdfm;
+SDdfmM = dfm - SDdfm;
+
+mvcdf1 = max(p7mvcdf(5500:9500));
+mvcdf2 = max(p7mvcdf(25500:29500));
+mvcdf3 = max(p7mvcdf(45500:49500));
+
+mvcdfSerial = max([mvcdf1, mvcdf2, mvcdf3]);
+mvcdf_Nm = abs(mvcdfSerial)*serial2lbs_bipolar*lbs2NmAt15cm;
+
+refsig = p7df_ref(1:2001);
+refsigser2Nm = mvcdf_Nm/4096*0.2;
+
+ref = refsig*refsigser2Nm;
+
+hold on;
+title('1/2 Hz Dorsiflexion');
+plot(dfm*serial2lbs_bipolar*lbs2NmAt15cm+8.25);
+plot(ref);
+plot(SDdfmP*serial2lbs_bipolar*lbs2NmAt15cm+8.25);
+plot(SDdfmM*serial2lbs_bipolar*lbs2NmAt15cm+8.25);

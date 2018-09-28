2b8f9b11220755eedc8740ea403d376d546ddc0f
Cory Jernigan <coryjernigan@corys-mbp-2.mobile.asu.edu>
2018-09-28 13:37:47 -0700

    code notes

--- ResearchGUI/Testdata/analyzeAnklebotData.m
+++ ResearchGUI/Testdata/analyzeAnklebotData.m
@@ -1,4 +1,16 @@
-data = load('AB_LCtest1.txt');
+% Ankle Bot load cell data
+data1 = load('AB_LCtest1.txt');
+data2 = load('AB_LCtest2.txt');
+data3 = load('AB_LCtest3.txt');
+data4 = load('AB_LCtest4.txt');
+
+% Our load cell data
+data5 = load('loadcelltest7.txt');
+data6 = load('loadcelltest8.txt');
+data7 = load('loadcelltest9.txt');
+
+data = data3; % the file to be analyzed 'data'
+
 
 %%
 meas = data(:,1);

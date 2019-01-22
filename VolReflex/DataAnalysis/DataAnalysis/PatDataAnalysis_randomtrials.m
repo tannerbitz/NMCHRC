81bf4176a34b57fa5068365b8c0bebf7893c0363
Tanner Bitz <tannerbitz@gmail.com>
2019-01-22 10:24:44 -0700

    added path changes to Data Analysis files

--- VolReflex/DataAnalysis/DataAnalysis/PatDataAnalysis_randomtrials.m
+++ VolReflex/DataAnalysis/DataAnalysis/PatDataAnalysis_randomtrials.m
@@ -1,7 +1,30 @@
 % Patient Data Analysis Script
+BASE_DIR = uigetdir(pwd, ...
+                    'Choose Patient Directory');
 
-path2add = sprintf('../Pat%s/', patNum);
-addpath(path2add);  % add path to data analysis functions
+% Retrieve Pat Number from path string                
+if ispc
+    pathparts = strsplit(BASE_DIR, '\');
+else
+    pathparts = strsplit(BASE_DIR, '/');
+end
+
+for i = 1:length(pathparts)
+    match = regexp(pathparts{i}, 'Pat(?<patNum>\d+)', 'names');
+    if ~isempty(match)
+        patNum = match.patNum;
+    end
+end
+
+% Add paths
+RD_DIR = fullfile(BASE_DIR, 'RawData'); %raw data folder
+PD_DIR = fullfile(BASE_DIR, 'ProcessedData'); %processed data folder
+if ~exist(PD_DIR, 'dir')
+    mkdir(PD_DIR)
+end
+addpath(BASE_DIR);
+addpath(RD_DIR);
+addpath(PD_DIR);
 
 matfile = sprintf('Pat%sMVC.mat', patNum);
 PatPF = cell(1);
@@ -21,15 +44,15 @@ for i = 0:length(freq) - 1
     PatPF{i*3 + 4} = analyzedata_randomtrials('matfile', matfile, 'vrfile', fname4);
 end
 
-for i = 1:length(PatPF)
-    fprintf('PF %i\n', i)
-    PatPF{i}.magphase = getMagPhase(PatPF{i}.nomrefcycle_nm, PatPF{i}.nommeascycle_nm);
-end
+% for i = 1:length(PatPF)
+%     fprintf('PF %i\n', i)
+%     PatPF{i}.magphase = getMagPhase(PatPF{i}.nomrefcycle_nm, PatPF{i}.nommeascycle_nm);
+% end
 
-pfvarname = sprintf('Pat%sPF', patNum);
-assignin('base', pfvarname, PatPF);
-patmatfile = sprintf('Pat%sPF_dataanalysis', patNum);
-save(patmatfile, pfvarname)
+% pfvarname = sprintf('Pat%sPF', patNum);
+% assignin('base', pfvarname, PatPF);
+% patmatfile = sprintf('Pat%sPF_dataanalysis', patNum);
+% save(patmatfile, pfvarname)
 
 %  DF
 PatDF = cell(1);
@@ -45,13 +68,13 @@ for i = 0:length(freq) - 1
     PatDF{i*3 + 4} = analyzedata_randomtrials('matfile', matfile, 'vrfile', fname4);
 end
 
-for i = 1:length(PatDF)
-    fprintf('DF %i\n', i)
-    PatDF{i}.magphase = getMagPhase(PatDF{i}.nomrefcycle_nm, PatDF{i}.nommeascycle_nm);
-end
-
-dfvarname = sprintf('Pat%sDF', patNum);
-assignin('base', dfvarname, PatDF);
-patmatfile = sprintf('Pat%sDF_dataanalysis', patNum);
-save(patmatfile, dfvarname)
+% for i = 1:length(PatDF)
+%     fprintf('DF %i\n', i)
+%     PatDF{i}.magphase = getMagPhase(PatDF{i}.nomrefcycle_nm, PatDF{i}.nommeascycle_nm);
+% end
+% 
+% dfvarname = sprintf('Pat%sDF', patNum);
+% assignin('base', dfvarname, PatDF);
+% patmatfile = sprintf('Pat%sDF_dataanalysis', patNum);
+% save(patmatfile, dfvarname)
 
--- VolReflex/DataAnalysis/DataAnalysis/analyzedata_randomtrials.m
+++ VolReflex/DataAnalysis/DataAnalysis/analyzedata_randomtrials.m
@@ -63,110 +63,113 @@ function res = analyzedata_randomtrials(varargin)
         save(procDataMat);
     end
     
-end  
-%     
-%     data = load(fullfile(trialpath, trialfile));
-%     refdata = data(:,refchannel);
-%     measdata = data(:, measchannel);
-%     zerolevel = mean(measdata(1:5000));
-%     measdata = measdata - zerolevel;
-%     
-%     % Take last cycle of refdata and find where it is zero (or at least
-%     % minimum).  This spot will be assumed to be 3/4 of a period ahead of
-%     % the start/stop of each trial period.  
-%     reflastperiod = refdata(end-ceil(trialstruct.samplesperperiod):end);
-%     periodmin = min(reflastperiod);
-%     minindices = find(reflastperiod==periodmin);
-% %     minindices = find(minindices > minindices(end) - trialstruct.samplesperperiod/2);
-%     minind = round(mean(minindices));
-%     minind = length(measdata) - (length(reflastperiod) - minind);
-%     
-%     zeroind = minind - 3/4*round(trialstruct.samplesperperiod);
-% 
-%     
-%     % Gather data into cycles
-%     numcycles = 15;
-%     cycles = zeros(numcycles, floor(trialstruct.samplesperperiod));
-%     startind = zeroind;
-%     for i = 1:numcycles
-%         actualind = zeroind - trialstruct.samplesperperiod*i;
-%         startind = startind - floor(trialstruct.samplesperperiod);
-%         stopind = startind + floor(trialstruct.samplesperperiod) - 1;
-%         inddiff = actualind - startind;
-%         if inddiff < -1
-%             startind = startind -1;
-%             stopind = stopind -1;
-%         end
-%         cycles(numcycles+1-i, :) = measdata(startind:stopind);
-%         refcycles(numcycles+1-i, :) = refdata(startind:stopind);
-%     end
-%     
-%     % Find mean and std deviation of cycles
-%     nomcycle = zeros(1, floor(trialstruct.samplesperperiod));
-%     stdcycle = zeros(1, floor(trialstruct.samplesperperiod));
-%     
-%     for i = 1:floor(trialstruct.samplesperperiod)
-%         nomcycle(i) = mean(cycles(:, i));
-%         stdcycle(i) = std(cycles(:, i));
-%     end
-%     lowerbound = nomcycle - 3*stdcycle;
-%     upperbound = nomcycle + 3*stdcycle;
-%     
-%     % Only keep data if all data points are within 3 std deviations of
-%     % mean
-%     k = 1;
-%     cycleslost = [];
-%     for j = 1:numcycles
-%         keepcycle = true;
-%         for i = 1:floor(trialstruct.samplesperperiod)
-%             if cycles(j, i) >= upperbound(i) || cycles(j, i) <= lowerbound(i)
-%                 keepcycle = false;
-%                 cycleslost(end + 1) = j;
-%                 break
-%             end
-%         end
-%         if keepcycle
-%             validcycles(k, :) = cycles(j, :);
-%             validrefcycles(k, :) = refcycles(j, :);
-%             k = k + 1;
-%         end
-%     end
-%     
-%     % recalculate nominal measured and reference cycles
-%     for i = 1:floor(trialstruct.samplesperperiod)
-%         nommeascycle(i) = mean(validcycles(:, i));
-%         nomrefcycle(i) = mean(validrefcycles(:,i));
-%     end
-%     
-%     % Normalize to MVC
-%     res2lbs = 250/4096;
-%     lbs2newtons =  4.44822;
-%     armlength = 0.15; % meters
-%     percentmvc = 0.3;
-%     
-%     if strcmp(trialstruct.flexion, 'DF')
-%         minrefval = 0;
-%         maxrefval = percentmvc*mvcdf;
-%         refsigspan = maxrefval - minrefval;
-%         nomrefcycle_nm = minrefval + nomrefcycle/4096*refsigspan;
-%     elseif strcmp(trialstruct.flexion, 'PF')
-%         minrefval = -percentmvc*mvcpf;
-%         maxrefval = 0;
-%         refsigspan = maxrefval - minrefval;
-%         nomrefcycle_nm = minrefval + nomrefcycle/4096*refsigspan;
-%     end
-%     
-%     nommeascycle_nm = nommeascycle*res2lbs*lbs2newtons*armlength;
-% 
-%     
-%     res = struct;
-%     res.allcycles = cycles;
-%     res.validcycles = validcycles;
-%     res.validrefcycles = validrefcycles;
-%     res.cycleslost = cycleslost;
-%     res.nomrefcycle_nm = nomrefcycle_nm;
-%     res.nommeascycle_nm = nommeascycle_nm;
-%     res.trialinfo = trialstruct;
-%     
-% 
-% end
+   % Load Data
+    data = load(fullfile(trialpath, trialfile));
+    refdata = data(:,refchannel);
+    measdata = data(:, measchannel);
+    
+    % There is a pause between 0-5s to start each trial.  Sometimes
+    % patients adjust during the first couple seconds, so take the
+    % zerolevel to be from 2-5s (i.e. sample 2000-5000)
+    zerolevel = mean(measdata(2000:5000)); 
+    measdata = measdata - zerolevel;
+    
+    % Take last cycle of refdata and find where it is zero (or at least
+    % minimum).  This spot will be assumed to be 3/4 of a period ahead of
+    % the start/stop of each trial period.  
+    reflastperiod = refdata(end-ceil(trialstruct.samplesperperiod):end);
+    periodmin = min(reflastperiod);
+    minindices = find(reflastperiod==periodmin);
+%     minindices = find(minindices > minindices(end) - trialstruct.samplesperperiod/2);
+    minind = round(mean(minindices));
+    minind = length(measdata) - (length(reflastperiod) - minind);
+    
+    zeroind = minind - 3/4*round(trialstruct.samplesperperiod);
+
+    
+    % Gather data into cycles
+    numcycles = 15;
+    cycles = zeros(numcycles, floor(trialstruct.samplesperperiod));
+    startind = zeroind;
+    for i = 1:numcycles
+        actualind = zeroind - trialstruct.samplesperperiod*i;
+        startind = startind - floor(trialstruct.samplesperperiod);
+        stopind = startind + floor(trialstruct.samplesperperiod) - 1;
+        inddiff = actualind - startind;
+        if inddiff < -1
+            startind = startind -1;
+            stopind = stopind -1;
+        end
+        cycles(numcycles+1-i, :) = measdata(startind:stopind);
+        refcycles(numcycles+1-i, :) = refdata(startind:stopind);
+    end
+    
+    % Find mean and std deviation of cycles
+    nomcycle = zeros(1, floor(trialstruct.samplesperperiod));
+    stdcycle = zeros(1, floor(trialstruct.samplesperperiod));
+    
+    for i = 1:floor(trialstruct.samplesperperiod)
+        nomcycle(i) = mean(cycles(:, i));
+        stdcycle(i) = std(cycles(:, i));
+    end
+    lowerbound = nomcycle - 3*stdcycle;
+    upperbound = nomcycle + 3*stdcycle;
+    
+    % Only keep data if all data points are within 3 std deviations of
+    % mean
+    k = 1;
+    cycleslost = [];
+    for j = 1:numcycles
+        keepcycle = true;
+        for i = 1:floor(trialstruct.samplesperperiod)
+            if cycles(j, i) >= upperbound(i) || cycles(j, i) <= lowerbound(i)
+                keepcycle = false;
+                cycleslost(end + 1) = j;
+                break
+            end
+        end
+        if keepcycle
+            validcycles(k, :) = cycles(j, :);
+            validrefcycles(k, :) = refcycles(j, :);
+            k = k + 1;
+        end
+    end
+    
+    % recalculate nominal measured and reference cycles
+    for i = 1:floor(trialstruct.samplesperperiod)
+        nommeascycle(i) = mean(validcycles(:, i));
+        nomrefcycle(i) = mean(validrefcycles(:,i));
+    end
+    
+    % Normalize to MVC
+    res2lbs = 250/4096;
+    lbs2newtons =  4.44822;
+    armlength = 0.15; % meters
+    percentmvc = 0.3;
+    
+    if strcmp(trialstruct.flexion, 'DF')
+        minrefval = 0;
+        maxrefval = percentmvc*mvcdf;
+        refsigspan = maxrefval - minrefval;
+        nomrefcycle_nm = minrefval + nomrefcycle/4096*refsigspan;
+    elseif strcmp(trialstruct.flexion, 'PF')
+        minrefval = -percentmvc*mvcpf;
+        maxrefval = 0;
+        refsigspan = maxrefval - minrefval;
+        nomrefcycle_nm = minrefval + nomrefcycle/4096*refsigspan;
+    end
+    
+    nommeascycle_nm = nommeascycle*res2lbs*lbs2newtons*armlength;
+
+    
+    res = struct;
+    res.allcycles = cycles;
+    res.validcycles = validcycles;
+    res.validrefcycles = validrefcycles;
+    res.cycleslost = cycleslost;
+    res.nomrefcycle_nm = nomrefcycle_nm;
+    res.nommeascycle_nm = nommeascycle_nm;
+    res.trialinfo = trialstruct;
+    
+
+end

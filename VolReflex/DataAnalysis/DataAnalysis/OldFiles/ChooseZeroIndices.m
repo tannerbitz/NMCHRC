c831363cb88824c4193b3feab0d27b3d1ef0b1a6
Tanner Bitz <tannerbitz@gmail.com>
2019-07-29 11:29:24 -0700

    updated gitignore

--- VolReflex/DataAnalysis/DataAnalysis/OldFiles/ChooseZeroIndices.m
+++
@@ -1,9 +0,0 @@
-function r = ChooseZeroIndices(d)
-    figure(1)
-    plot(d)
-    
-    xnum = input('How many zero regions?\n');
-        
-    [x, y] = ginput(xnum*2);
-    r = x;
-end
--- VolReflex/ResearchGUI/PatientData/SineData/Pat7/sumsqr.m
+++
@@ -1,6 +0,0 @@
-function SE  = sumsqr(m)
-    SE = 0;
-    for i = 1:length(m)
-        SE = SE + m(i)^2;
-    end
-end

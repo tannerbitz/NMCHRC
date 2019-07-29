e97ac33928fc6341c19690cbe2f128c09efd1917
Carly Thalman <cmthalma@sundevils.asu.edu>
2019-07-29 14:28:03 -0700

    input meas and ref channel added to mvc script

--- VolReflex/DataAnalysis/GetMVC/CalculateMVC.m
+++ VolReflex/DataAnalysis/GetMVC/CalculateMVC.m
@@ -2,9 +2,15 @@ clear all;
 close all;
 clc;
 
+measuredchannel = input('Enter Measured Channel Number\n');
+referencechannel = input('Enter Reference Channel Number\n');
 
-PF = getmvc();
+fprintf('Choose Plantarflexion MVC Text File\n')
+PF = getmvc('MeasChan', measuredchannel+1, ...
+            'RefChan', referencechannel+1);
 close all
-DF = getmvc();
+fprintf('Choose Dorsiflexion MVC Text File\n')
+DF = getmvc('MeasChan', measuredchannel+1, ...
+            'RefChan', referencechannel+1);
 matfname = input('Enter MAT filename in quotes(no extension needed)\n');
 save(matfname, 'PF', 'DF')
